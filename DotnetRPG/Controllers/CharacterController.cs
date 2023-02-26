600c1a43c894c6d760ecba7c0a8a83805f87d260
Mata07 <mmatisic7@gmail.com>
2023-02-26 22:46:33 +0100

    Added GetSingle Action

--- DotnetRPG/Controllers/CharacterController.cs
+++ DotnetRPG/Controllers/CharacterController.cs
@@ -17,42 +17,18 @@ namespace DotnetRPG.Controllers
             new Character { Name = "Sam"}
         };
 
+        //[Route("GetAll")]
+        [HttpGet("GetAll")] // Route i action u jednom
         public IActionResult Get()
         {
             return Ok(characters);
         }
 
+        public IActionResult GetSingle()
+        {
+            return Ok(characters[0]);
+        }
 
-        //// GET: api/<CharacterController>
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
 
-        //// GET api/<CharacterController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/<CharacterController>
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        //// PUT api/<CharacterController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/<CharacterController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
