c6d40ad5b04739e941555564d43b6a3b75c40693
Mata07 <mmatisic7@gmail.com>
2023-03-02 00:26:43 +0100

    AuthenticationRepository

---
+++ DotnetRPG/Services/AuthenticationService/AuthRepository.cs
@@ -0,0 +1,67 @@
+﻿using DotnetRPG.Data;
+using DotnetRPG.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DotnetRPG.Services.AuthenticationService
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        
+
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            // check if user exists in db
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            if(await UserExists(user.Username))
+            {
+                response.Success = false;
+                response.Message = "User already exists.";
+                return response;
+            }
+
+            // Hash password
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            // Set on user object
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            response.Data = user.Id;
+            return response;    //send user.Id
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if(await _context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        // sa out keyword ne moramo return nego out automatski vraća
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) 
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
---
+++ DotnetRPG/Services/AuthenticationService/IAuthRepository.cs
@@ -0,0 +1,12 @@
+﻿using DotnetRPG.Models;
+using System.Threading.Tasks;
+
+namespace DotnetRPG.Services.AuthenticationService
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);    // returns int Id of User
+        Task<ServiceResponse<string>> Login(string username, string password);  //returns token as string
+        Task<bool> UserExists(string username);
+    }
+}
