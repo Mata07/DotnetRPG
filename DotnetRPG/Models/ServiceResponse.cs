0e8d5ff0262b267b8c787141c0c9318f534d3731
Mata07 <mmatisic7@gmail.com>
2023-03-01 01:56:22 +0100

    Added ServiceResponse

---
+++ DotnetRPG/Models/ServiceResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace DotnetRPG.Models
+{
+    // Wrapper za slanje dodatnih informacija ili poruka za exception
+    // ili za frontend poruke
+    public class ServiceResponse<T>
+    {
+        // Body data tipa T
+        public T Data { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = null;
+    }
+}
--- DotnetRPG/Services/CharacterService/ICharacterService.cs
+++ DotnetRPG/Services/CharacterService/ICharacterService.cs
@@ -6,8 +6,10 @@ namespace DotnetRPG.Services.CharacterService
 {
     public interface ICharacterService
     {
-        Task<List<Character>> GetAllCharacters();
-        Task<Character> GetCharacterById(int id);
-        Task<List<Character>> AddCharacter(Character newCharacter);
+        //Task<List<Character>> GetAllCharacters();
+        // Napravili smo wrapper ServiceResponse i dodali ga
+        Task<ServiceResponse<List<Character>>> GetAllCharacters();
+        Task<ServiceResponse<Character>> GetCharacterById(int id);
+        Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
     }
 }
